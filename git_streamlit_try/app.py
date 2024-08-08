ab22ccab06640dc8ec9055b9e26bafbb48520c64
kshitizkamra <68267771+kshitizkamra@users.noreply.github.com>
2024-08-08 12:10:37 +0530

    Update app.py

--- git_streamlit_try/app.py
+++ git_streamlit_try/app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import sys
+import glob
+import os
+from sqlalchemy import create_engine,MetaData,Table, Column, Numeric, Integer, VARCHAR, update
+import altair as alt
+import plotly.express as px
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.connection('marketplace_analysis_db', type='sql')
 
-conn
+conn = st.connection("my_database")
+
+db_settlement_1234=conn.query("select * from trial_1;")
+db_settlement_1234
